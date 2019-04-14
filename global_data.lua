fc6bc2dfbcd38aad29c85fecdbc1a7f5791ee104
Derek O'Harrow <38404530+derekoharrow@users.noreply.github.com>
2019-04-14 14:08:15 +0100

    Create global_data.lua

---
+++ global_data.lua
@@ -0,0 +1,12 @@
+return {
+	helpers = {},
+	data = {
+		TadoToken  = { initial = '' },
+		TadoHomeId = { initial = '' },
+		TadoZones = { initial = '' },
+		TadoUsers = { initial = '' },
+		TadoClientId = { initial = 'tado-web-app' },															-- Check at https://my.tado.com/webapp/env.js
+		TadoClientSecret = { initial = 'wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc' },	-- Check at https://my.tado.com/webapp/env.js
+		TadoUserMapping = { initial = { ["Ryan O'Harrow"] = "Ryan's bedroom", ["Lauren O'Harrow"] = "Lauren's bedroom", ["Lindsey O'Harrow"] = "Sewing room" } }
+	}
+}
